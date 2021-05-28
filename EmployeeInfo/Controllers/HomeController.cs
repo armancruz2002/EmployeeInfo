6f54b28ae3625e6ea0b8179701be1f7a803614ec
Arman Cruz <armancruz2002@yahoo.com>
2021-05-29 00:17:35 +0800

    Add project files.

---
+++ EmployeeInfo/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using EmployeeInfo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Data;
+using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EmployeeInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {                      
+            return View();
+        }
+        public IActionResult Data_entry()
+        {
+            EmployeeController model = new EmployeeController();
+            DataTable dt = model.GetAllEmployee();
+            return View("Data_entry", dt);        
+        }
+
+        public ActionResult InsertRecord(Microsoft.AspNetCore.Http.IFormCollection frm, string action)
+        {
+                EmployeeController model = new EmployeeController();
+                string FirstName = frm["FirstName"];                 
+                string MiddleName = frm["MiddleName"];
+                string LastName = frm["LastName"];              
+                int status = model.InsertEmployee(FirstName,  MiddleName, LastName);
+                return RedirectToAction("Data_entry");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            EmployeeController model = new EmployeeController();
+            DataTable dt = model.GetEmployeeInfo(Id);
+            return View("Edit_record", dt);
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            EmployeeController model = new EmployeeController();
+            DataTable dt = model.GetEmployeeInfo(Id);
+            return View("Delete_record", dt);
+        }
+
+        
+
+        public ActionResult SaveUpdate(Microsoft.AspNetCore.Http.IFormCollection frm, string action)
+        {
+            EmployeeController model = new EmployeeController();
+            int Id = Convert.ToInt32(frm["Id"]); 
+            string FirstName = frm["FirstName"];
+            string MiddleName = frm["MiddleName"];
+            string LastName = frm["LastName"];
+            int status = model.UpdateEmployee(Id, FirstName, MiddleName, LastName);
+            return RedirectToAction("Data_entry");
+        }
+
+        public ActionResult DeleteRecord(Microsoft.AspNetCore.Http.IFormCollection frm, string action)
+        {
+            EmployeeController model = new EmployeeController();
+            int Id = Convert.ToInt32(frm["Id"]);
+            int status = model.DeleteEmployee(Id);
+            return RedirectToAction("Data_entry");
+        }
+
+
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+
+        public IActionResult Insert_record()
+        {
+            return View();
+        }
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EmployeeInfo/Models/Employee.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeInfo.Models
+{   
+    public class EmployeeController
+    {
+        string connectionString = @"Data Source=DESKTOP-OE5C2KJ\SQLEXPRESS;Initial Catalog=EmployeeRecords;Integrated Security=True";
+
+        public DataTable GetAllEmployee()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from employee", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public DataTable GetEmployeeInfo(int Id)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Employee where Id=" + Id, con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public int UpdateEmployee(int Id, string FirstName, string MiddleName, string LastName)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Update Employee SET FirstName=@FirstName, MiddleName=@MiddleName , LastName=@LastName where Id=@Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@FirstName", FirstName);
+                cmd.Parameters.AddWithValue("@MiddleName", MiddleName);
+                cmd.Parameters.AddWithValue("@LastName", LastName);
+                cmd.Parameters.AddWithValue("@Id", Id);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int InsertEmployee(string FirstName, string MiddleName, string LastName)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Insert into Employee(FirstName, MiddleName, LastName) values(@FirstName, @MiddleName, @LastName)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@FirstName", FirstName);
+                cmd.Parameters.AddWithValue("@MiddleName", MiddleName);
+                cmd.Parameters.AddWithValue("@LastName", LastName);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int DeleteEmployee(int Id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Delete from Employee where Id=@Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Id", Id);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
